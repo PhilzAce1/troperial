d29899ac0d2c6e73ebb4cdc8ac6f4c8acc435e0b
Philzace1 <unknownumeh@gmail.com>
2020-06-01 19:03:04 +0100

    amplify setup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -45,7 +45,11 @@ const SignUp = () => {
       const newUser = await Auth.signUp({
         username: email,
         password,
+        attributes: {
+          email, // optional
+        },
       });
+      console.log(newUser);
       setIsLoading(false);
       setNewUser(newUser);
     } catch (e) {
