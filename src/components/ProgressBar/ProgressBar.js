de9f998a6980c7345dbb14ad3bee8dbb493d2e36
Runo-saduwa <orunosaduwa@gmail.com>
2020-06-24 23:56:37 +0100

    password meter rework

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './ProgressBar.css';
+import validator from 'validator'
+import { passwordRegex } from '../../constants/regex';
+const ProgressBar = ({ value }) => {
+  const testedResult = (password, reg = passwordRegex) => {
+    if(!validator.matches(password, reg)){
+      if(password.length > 20){
+        return {quality: 'Invalid', percentage: 100}
+      }
+      return {quality: 'Weak', percentage: 1}
+    } else {
+      if(password.length < 8) {
+        return {quality: 'Medium', percentage: 50}
+      }else {
+       return {quality: 'Strong', percentage: 80}
+      }
+    }
+  } 
+  const grade = testedResult(value, passwordRegex);
 
-const ProgressBar = ({ grade }) => {
   return (
     <div className="progressBar__container">
-      {grade === null ? null : (
-        <>
-          <p className="progressBar__title">Password Strength</p>
-          <progress
-            className={`${grade.quality}`}
-            max="100"
-            value={grade.percentage}
-          ></progress>
-          <p className="progressBar__grade">{grade.quality}</p>
-        </>
-      )}
+        {value && (
+          <Fragment>
+              <p className="progressBar__title">Password Strength</p>
+            {value}
+            <progress
+              className={`${grade.quality}`}
+              max="100"
+              value={grade.percentage}
+            ></progress>
+            <p className="progressBar__grade">{grade.quality}</p>
+          </Fragment>
+        )}
     </div>
   );
 };
