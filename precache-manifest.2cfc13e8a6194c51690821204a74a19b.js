281f917833bf477ee47b4490fccf816e9070cb69
Philzace1 <unknownumeh@gmail.com>
2020-07-17 01:11:03 +0100

    Updates

---
+++ precache-manifest.2cfc13e8a6194c51690821204a74a19b.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22e903c701224344280b1e23e264ad6",
+    "url": "/troperial/index.html"
+  },
+  {
+    "revision": "9c9293ed634ddf0bdfd2",
+    "url": "/troperial/static/css/2.a727fa6c.chunk.css"
+  },
+  {
+    "revision": "c179c613803a498d0efe",
+    "url": "/troperial/static/css/main.da956be6.chunk.css"
+  },
+  {
+    "revision": "9c9293ed634ddf0bdfd2",
+    "url": "/troperial/static/js/2.f350b226.chunk.js"
+  },
+  {
+    "revision": "76fcc23234dd594ebf4b9aa8b2585a08",
+    "url": "/troperial/static/js/2.f350b226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c179c613803a498d0efe",
+    "url": "/troperial/static/js/main.b4b0bba5.chunk.js"
+  },
+  {
+    "revision": "c749d228b45781a35019",
+    "url": "/troperial/static/js/runtime-main.e91a852b.js"
+  },
+  {
+    "revision": "e903aa70818f56aa9d2b6bc79f2f6ee5",
+    "url": "/troperial/static/media/Logo.e903aa70.svg"
+  },
+  {
+    "revision": "d8ba0fb2a631d43223f2bda0a5ed68cb",
+    "url": "/troperial/static/media/USD.d8ba0fb2.png"
+  },
+  {
+    "revision": "66a0e0d099637bbeb77537062788a212",
+    "url": "/troperial/static/media/all-listings.66a0e0d0.PNG"
+  },
+  {
+    "revision": "6eddee732441fad2f529a77053dc804a",
+    "url": "/troperial/static/media/bypass-1.6eddee73.svg"
+  },
+  {
+    "revision": "01bdfef50f39867d93c96242952db5b0",
+    "url": "/troperial/static/media/bypass-2.01bdfef5.svg"
+  },
+  {
+    "revision": "2521e18e04f86ac6e8982a8ec5a17a23",
+    "url": "/troperial/static/media/cad.2521e18e.svg"
+  },
+  {
+    "revision": "4200304a278a02f8c104e351fddf5dcd",
+    "url": "/troperial/static/media/cash-app.4200304a.svg"
+  },
+  {
+    "revision": "0cd331e6603c3c90685578fa26d65159",
+    "url": "/troperial/static/media/chat-platform.0cd331e6.svg"
+  },
+  {
+    "revision": "54b9dbd3e39f5d6a7caa47a60313ba47",
+    "url": "/troperial/static/media/chat-svg.54b9dbd3.svg"
+  },
+  {
+    "revision": "d658d26540261df32fd27dc4f56ab5a8",
+    "url": "/troperial/static/media/create-account.d658d265.svg"
+  },
+  {
+    "revision": "4d6b09edd7b4835d9387458c90ed4f5d",
+    "url": "/troperial/static/media/email-icon.4d6b09ed.svg"
+  },
+  {
+    "revision": "30bf200d708c613a3ef967ec102bcfb8",
+    "url": "/troperial/static/media/exchange-troperial-logo.30bf200d.png"
+  },
+  {
+    "revision": "aaa3ed375454346e7d541d5401410c96",
+    "url": "/troperial/static/media/facebook.aaa3ed37.svg"
+  },
+  {
+    "revision": "80bc05a5754ccfbb6588c5074fcf9c9e",
+    "url": "/troperial/static/media/forgot-password-svg.80bc05a5.PNG"
+  },
+  {
+    "revision": "7acacaf3524e8dec6be096a35cc8c388",
+    "url": "/troperial/static/media/green-caret.7acacaf3.svg"
+  },
+  {
+    "revision": "57105403f9712931e6cd19ccdb617ad1",
+    "url": "/troperial/static/media/green-check.57105403.svg"
+  },
+  {
+    "revision": "61ee411070797654315f81cf762ce3bb",
+    "url": "/troperial/static/media/gt-walsheim-bold-web.61ee4110.ttf"
+  },
+  {
+    "revision": "9b7721bb8734ff33f9ed62918493ccc4",
+    "url": "/troperial/static/media/gt-walsheim-light-web.9b7721bb.ttf"
+  },
+  {
+    "revision": "1821054ee3a6efc4213a6edece2e96b1",
+    "url": "/troperial/static/media/gt-walsheim-medium-web.1821054e.ttf"
+  },
+  {
+    "revision": "a78cebccad7eac33f080875974108711",
+    "url": "/troperial/static/media/gt-walsheim-web.a78cebcc.ttf"
+  },
+  {
+    "revision": "a768e45928cfddf5c1d62a421d94d308",
+    "url": "/troperial/static/media/hamburger-bars.a768e459.svg"
+  },
+  {
+    "revision": "d18990279a1b8d994e6143fec1a9e817",
+    "url": "/troperial/static/media/instagram.d1899027.svg"
+  },
+  {
+    "revision": "351fb75321b00dfcd5ad7212a3ff4ff0",
+    "url": "/troperial/static/media/layout-horizontal-svg.351fb753.svg"
+  },
+  {
+    "revision": "fe34058430f53badea059aa5024602e0",
+    "url": "/troperial/static/media/linkedIn.fe340584.svg"
+  },
+  {
+    "revision": "153223c92d3a375b0eae7571328a8394",
+    "url": "/troperial/static/media/minus.153223c9.svg"
+  },
+  {
+    "revision": "45853e84235517b14a174934a0ea2159",
+    "url": "/troperial/static/media/money-bag.45853e84.png"
+  },
+  {
+    "revision": "1e3e7db029986f834b8494e7d8e1ad8c",
+    "url": "/troperial/static/media/news-sample-picture.1e3e7db0.PNG"
+  },
+  {
+    "revision": "3cce05a2b45968253b7e0fe2274d49aa",
+    "url": "/troperial/static/media/notifications-svg.3cce05a2.svg"
+  },
+  {
+    "revision": "255e24fe4e8f531d626849d6c0ea2968",
+    "url": "/troperial/static/media/person1.255e24fe.png"
+  },
+  {
+    "revision": "37441f2f9c068a3d596dd00faec9d186",
+    "url": "/troperial/static/media/person2.37441f2f.png"
+  },
+  {
+    "revision": "1a04778a2ca65e43ce8b3ca289032582",
+    "url": "/troperial/static/media/person3.1a04778a.png"
+  },
+  {
+    "revision": "ae121478193f05ea69d5ad741f0027c4",
+    "url": "/troperial/static/media/phone-action.ae121478.svg"
+  },
+  {
+    "revision": "cf7c0e27d28d63b87209696746e4b339",
+    "url": "/troperial/static/media/platform-listings-group.cf7c0e27.svg"
+  },
+  {
+    "revision": "60ad3bbb618655ba73d415e0302ee80c",
+    "url": "/troperial/static/media/platform-summary-group.60ad3bbb.svg"
+  },
+  {
+    "revision": "9b23d957eaa0916e1c2f1559407b99af",
+    "url": "/troperial/static/media/plus.9b23d957.svg"
+  },
+  {
+    "revision": "f780b23ea026310172b7a8310bb4e1b7",
+    "url": "/troperial/static/media/profile-page-svg.f780b23e.svg"
+  },
+  {
+    "revision": "e2e6ba75ae1a898144a59913a7fd9630",
+    "url": "/troperial/static/media/reset-password-illus.e2e6ba75.PNG"
+  },
+  {
+    "revision": "79cc101dfc3ff3c8b8b64f6f940c5fd3",
+    "url": "/troperial/static/media/reset-password-svg.79cc101d.PNG"
+  },
+  {
+    "revision": "9fb0e5b93cea112b516f4a8dd4f61020",
+    "url": "/troperial/static/media/send-icon.9fb0e5b9.svg"
+  },
+  {
+    "revision": "e08fa1ee87247a047bed290f6526513b",
+    "url": "/troperial/static/media/success-wealth.e08fa1ee.svg"
+  },
+  {
+    "revision": "658a508a7ce4ccad6180e418c258d354",
+    "url": "/troperial/static/media/thunder-bolt.658a508a.svg"
+  },
+  {
+    "revision": "54d7c5f838591128e2ed2878d4848a0b",
+    "url": "/troperial/static/media/trading-card.54d7c5f8.svg"
+  },
+  {
+    "revision": "099fb577b5440bee96759d44e375d8a1",
+    "url": "/troperial/static/media/troperial-delete.099fb577.PNG"
+  },
+  {
+    "revision": "4fd6b24cea5caa4e290ea323f350e6e2",
+    "url": "/troperial/static/media/troperial-email-illus.4fd6b24c.PNG"
+  },
+  {
+    "revision": "26fbad5ef839299a5b67b9f4cea9fb50",
+    "url": "/troperial/static/media/troperial-mobile-verified.26fbad5e.PNG"
+  },
+  {
+    "revision": "0af6b1befb451d72909d6d04b10f3c7f",
+    "url": "/troperial/static/media/troperial-profile-aside.0af6b1be.PNG"
+  },
+  {
+    "revision": "1d6a711a66b49ab5e6d626613e175b4a",
+    "url": "/troperial/static/media/troperial-sign-in-svg.1d6a711a.PNG"
+  },
+  {
+    "revision": "17b37eedc69191f036abaae7c07c45e7",
+    "url": "/troperial/static/media/troperial-verified.17b37eed.PNG"
+  },
+  {
+    "revision": "5569f1c1d6e0cdae833cd54569f54625",
+    "url": "/troperial/static/media/twitter.5569f1c1.svg"
+  },
+  {
+    "revision": "9dea6ebdc1ebed93b0c6a575b60cfdd9",
+    "url": "/troperial/static/media/update-status.9dea6ebd.svg"
+  },
+  {
+    "revision": "d13a226ba79f2dbfaad9e470156b9113",
+    "url": "/troperial/static/media/zelle.d13a226b.svg"
+  }
+]);
