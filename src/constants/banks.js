1514d61ea58d87e7895bff09dafd990af8f821ec
Runo-saduwa <orunosaduwa@gmail.com>
2020-07-20 23:22:57 +0100

    updates add bank account

---
+++ src/constants/banks.js
@@ -0,0 +1,330 @@
+export const optionsUS = [
+  {
+    label: 'JPMorgan Chase',
+    value: 'JPMorgan Chase',
+  },
+  {
+    label: 'Bank of America',
+    value: 'Bank of America',
+  },
+  {
+    label: 'Citigroup',
+    value: 'Citigroup',
+  },
+  {
+    label: 'Wells Fargo',
+    value: 'Wells Fargo',
+  },
+  {
+    label: 'TD Bank',
+    value: 'TD Bank',
+  },
+  {
+    label: 'Capital One',
+    value: 'Capital One',
+  },
+  {
+    label: 'HSBC Bank USA',
+    value: 'HSBC Bank USA',
+  },
+  {
+    label: 'USAA',
+    value: 'USAA',
+  },
+  {
+    label: 'Citizens Financial Group',
+    value: 'Citizens Financial Group',
+  },
+  {
+    label: 'Santander Bank',
+    value: 'Santander Bank',
+  },
+  {
+    label: 'Barclays',
+    value: 'Barclays',
+  },
+  {
+    label: 'RBC Bank',
+    value: 'RBC Bank',
+  },
+  {
+    label: 'UBS',
+    value: 'UBS',
+  },
+  {
+    label: 'M&T Bank',
+    value: 'M&T Bank',
+  },
+  {
+    label: 'First Republic Bank',
+    value: 'First Republic Bank',
+  },
+  {
+    label: 'Silicon Valley Bank',
+    value: 'Silicon Valley Bank',
+  },
+  {
+    label: 'New York Community Bank',
+    value: 'New York Community Bank',
+  },
+  {
+    label: 'Valley National Bank',
+    value: 'Valley National Bank',
+  },
+  {
+    label: 'Investors Bank',
+    value: 'Investors Bank',
+  },
+];
+export const optionsUK =[
+  {
+    label: "HSBC",
+    value: "HSBC",
+  },
+  {
+    label: "Royal Bank of Scotland",
+    value: "Royal Bank of Scotland",
+  },
+  {
+    label: "NatWest",
+    value: "NatWest",
+  },
+  {
+    label: "Lloyds TSB",
+    value: "Lloyds TSB",
+  },
+  {
+    label: "Halifax",
+    value: "Halifax",
+    
+  },
+  {
+    label: "Bank of Scotland",
+    value: "Bank of Scotland",
+  },
+  {
+    label: "Barclays",
+    value: "Barclays",
+  },
+  {
+    label: "Santander",
+    value: "Santander",
+  },
+  {
+    label: "Co-operative",
+    value: "Co-operative",
+  },
+  {
+    label: "Nationwide",
+    value: "Nationwide",
+  },
+  {
+    label: "Tesco bank",
+    value: "Tesco bank"
+  }
+]
+
+export const optionsCD = [
+  {
+    label: "Royal Bank of Canada",
+    value: "Royal Bank of Canada",
+  },
+  {
+    label: "Toronto-Dominion Bank",
+    value: "Toronto-Dominion Bank"
+  },
+  {
+    label: "Bank of Nova Scotia",
+    value: "Bank of Nova Scotia"
+  },
+  {
+    label: "Bank of Montreal",
+    value: "Bank of Montreal"
+  },
+  {
+    label: "Canadian Imperial Bank of Commerce",
+    value:"Canadian Imperial Bank of Commerce"
+  }
+]
+
+export const optionsNG = [
+  {
+    value: 'Access Bank',
+    label: 'Access Bank',
+  },
+  {
+    value: 'Citibank',
+    label: 'Citibank',
+  },
+  {
+    label: 'Diamond Bank',
+    value: 'Diamond Bank',
+  },
+  {
+    value: 'Dynamic Standard Bank',
+    label: 'Dynamic Standard Bank',
+  },
+  {
+    label: 'Ecobank Nigeria',
+    value: 'Ecobank Nigeria',
+  },
+  {
+    label: 'Fidelity Bank Nigeria',
+    value: 'Fidelity Bank Nigeria',
+  },
+  {
+    value: 'First Bank of Nigeria',
+    label: 'First Bank of Nigeria',
+  },
+  {
+    label: 'First City Monument Bank',
+    value: 'First City Monument Bank',
+  },
+  {
+    label: 'Guaranty Trust Bank',
+    value: 'Guaranty Trust Bank',
+  },
+  {
+    label: 'Heritage Bank Plc',
+    value: 'Heritage Bank Plc',
+  },
+  {
+    label: 'Jaiz Bank',
+    value: 'Jaiz Bank',
+  },
+  {
+    label: 'Keystone Bank Limited',
+    value: 'Keystone Bank Limited',
+  },
+  {
+    label: 'Providus Bank Plc',
+    value: 'Providus Bank Plc',
+  },
+  {
+    label: 'Polaris Bank',
+    value: 'Polaris Bank',
+  },
+  {
+    label: 'Stanbic IBTC Bank Nigeria Limited',
+    value: 'Stanbic IBTC Bank Nigeria Limited',
+  },
+  {
+    label: 'Standard Chartered Bank',
+    value: 'Standard Chartered Bank',
+  },
+  {
+    label: 'Sterling Bank',
+    value: 'Sterling Bank',
+  },
+  {
+    label: 'Suntrust Bank Nigeria Limited',
+    value: 'Suntrust Bank Nigeria Limited',
+  },
+  {
+    label: 'Union Bank of Nigeria',
+    value: 'Union Bank of Nigeria',
+  },
+  {
+    label: 'United Bank for Africa',
+    value: 'United Bank for Africa',
+  },
+  {
+    label: 'Unity Bank Plc',
+    value: 'Unity Bank Plc',
+  },
+  {
+    label: 'Wema Bank',
+    value: 'Wema Bank',
+  },
+  {
+    label: 'Zenith Bank',
+    value: 'Zenith Bank',
+  },
+];
+
+export const optionsEU = [
+  {
+    label: "HSBC",
+    value: "HSBC",
+  },
+  {
+    label: "BNP Paribas",
+    value: "BNP Paribas"
+  },
+  {
+    label: "Crédit Agricole",
+    value: "Crédit Agricole",
+  },
+  {
+    label: "Deutsche Bank",
+    value: "Deutsche Bank",
+  },
+  {
+    label: "Banco Santander",
+    value: "Banco Santander",
+  },
+  {
+    label: "Barclays",
+    value: "Barclays",
+  },
+  {
+    label: "Société Générale",
+    value: "Société Générale",
+  },
+  {
+    label: "Groupe BPCE",
+    value: "Groupe BPCE",
+  },
+  {
+    label: "Lloyds Banking Group",
+    value: "Lloyds Banking Group"
+  },
+  {
+    label: "ING Group",
+    value: "ING Group",
+  },
+  {
+    label: "UniCredit",
+    value: "UniCredit"
+  },
+  {
+    label: "Royal Bank of Scotland Group",
+    value: "Royal Bank of Scotland Group"
+  },
+  {
+    label: "Intesa Sanpaolo",
+    value: "Intesa Sanpaolo",
+  },
+  {
+    label: "Crédit Mutuel Group",
+    value: "Crédit Mutuel Group",
+  },
+  {
+    label: "UBS",
+    value: "UBS",
+  },
+  {
+    label: "Credit Suisse",
+    value: "Credit Suisse",
+  },
+  {
+    label: "Banco Bilbao Vizcaya Argentaria",
+    value: "Banco Bilbao Vizcaya Argentaria",
+  },
+  {
+    label: "Rabobank",
+    value: "Rabobank"
+  },
+  {
+    label: "Nordea",
+    value: "Nordea",
+  },
+  {
+    label: "Standard Chartered",
+    value: "Standard Chartered",
+  },
+  {
+    label: "NatWest",
+    value: "NatWest",
+  }
+]
--- src/containers/AddBankAccout/AddBankAccount.js
+++ src/containers/AddBankAccout/AddBankAccount.js
@@ -4,6 +4,7 @@ import CustomButton from '../../components/CustomButton/CustomButton';
 import HybridInput from '../../components/HybridInput/HybridInput';
 import InputError from '../../components/InputError/InputError';
 import { currency_titles } from '../../constants/currency_titles';
+import { optionsUS, optionsNG, optionsUK, optionsEU, optionsCD } from '../../constants/banks';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { getAccount } from '../../actions/authActions';
 import { ToastContainer, toast } from 'react-toastify';
@@ -12,20 +13,45 @@ import 'react-toastify/dist/ReactToastify.css';
 import './AddBankAccount.css';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import Select from 'react-select';
 import { connect } from 'react-redux';
 import { emailRegex } from '../../constants/regex';
 const AddBankAccount = ({ accountId, getAccount}) => {
   const { register, handleSubmit, errors } = useForm();
   const [country, setCountry] = useState('USD');
+  const [selectedBank, setSelectedBank] = useState(null);
+  const [accountType, setAccountType] = useState(null);
 
-  const changeCurrencyHandler = (data) => {
-    setCountry(data);
+  const accountTypeOpts = [
+    {
+      label: 'CURRENT',
+      value: 'CURRENT'
+    },
+    {
+      label: 'SAVINGS',
+      value: 'SAVINGS'
+    }
+  ]
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      borderRadius: '10px',
+      marginTop: '10px',
+      padding: '4px 0',
+      borderWidth:'2px',
+      borderColor: "#e8e8e8"
+    }),
   };
+
+  const changeCurrencyHandler = (data) => setCountry(data);
+  const handleSelectedBank = selectedOption =>  setSelectedBank(selectedOption);
+  const handleAccountType = selectedOption =>  setAccountType(selectedOption);
+
 
 
   const submitNGNAccount = async (data) => {
     const authToken = localStorage.getItem('authToken');
-    const { accountName, bvnNumber, accountNumber, primaryBank } = data;
+    const { accountName, bvnNumber, accountNumber} = data;
 
     try {
         await axios.post(
@@ -33,9 +59,9 @@ const AddBankAccount = ({ accountId, getAccount}) => {
         {
           bvnNumber,
           accountNumber,
-          primaryBank,
+          primaryBank: selectedBank.value,
           accountName,
-          externalAccountSubType: 'CURRENT',
+          externalAccountSubType: accountType.value,
         },
         {
           headers: {
@@ -44,9 +70,13 @@ const AddBankAccount = ({ accountId, getAccount}) => {
         },
       );
       toast.success('Account Successfully Added!')
+      setSelectedBank(null);
+      setAccountType(null)
       getAccount(accountId)
     } catch (e) {
       console.log(e);
+      setSelectedBank(null);
+      setAccountType(null)
       toast.error('Oops something went wrong, please try again!')
     }
   };
@@ -99,16 +129,17 @@ const AddBankAccount = ({ accountId, getAccount}) => {
   }
   const submitUSDAccount = async (data) => {
     const authToken = localStorage.getItem('authToken');
-    const { routingNumber, accountNumber, primaryBank } = data;
+    const { accountName, routingNumber, accountNumber } = data;
 
     try {
        await axios.post(
         `https://accounts.api.troperial.com/accounts/${accountId}/externalAccounts/us`,
         {
           routingNumber,
+          accountName,
           accountNumber,
-          primaryBank,
-          externalAccountSubType: 'CURRENT',
+          primaryBank: selectedBank.value,
+          externalAccountSubType: accountType.value,
         },
         {
           headers: {
@@ -117,15 +148,19 @@ const AddBankAccount = ({ accountId, getAccount}) => {
         },
       );
       toast.success('Account Successfully Added!')
+      setSelectedBank(null);
+      setAccountType(null)
       getAccount(accountId)
     } catch (e) {
       console.log(e);
       toast.error('Oops something went wrong, please try again!')
+      setSelectedBank(null);
+      setAccountType(null)
     }
   };
   const submitUKAccount = async (data) => {
     const authToken = localStorage.getItem('authToken');
-    const { customerAccountNumber, sortCode, primaryBank } = data;
+    const { accountName, customerAccountNumber, sortCode  } = data;
 
     try {
          await axios.post(
@@ -133,8 +168,9 @@ const AddBankAccount = ({ accountId, getAccount}) => {
         {
           customerAccountNumber,
           sortCode,
-          primaryBank,
-          externalAccountSubType: 'CURRENT',
+          primaryBank: selectedBank.value,
+          accountName,
+          externalAccountSubType: accountType.value,
         },
         {
           headers: {
@@ -143,10 +179,14 @@ const AddBankAccount = ({ accountId, getAccount}) => {
         },
       );
       toast.success('Account Successfully Added!')
+      setSelectedBank(null);
+      setAccountType(null)
       getAccount(accountId)
     } catch (e) {
       console.log(e);
       toast.error('Oops something went wrong, please try again!')
+      setSelectedBank(null);
+      setAccountType(null)
     }
   };
 
@@ -177,21 +217,8 @@ const AddBankAccount = ({ accountId, getAccount}) => {
           name="bvnNumber"
           label="BVN"
         />
-        {errors.primaryBank?.type === 'required' && (
-          <InputError>Your bank name is required</InputError>
-        )}
-        <CustomInput
-          placeholder="Bank Name"
-          showError={errors.primaryBank ? true : false}
-          register={register({
-            required: true,
-          })}
-          name="primaryBank"
-          label="Bank Name"
-        />
-        {errors.accountName?.type === 'required' && (
-          <InputError>Your account name is required</InputError>
-        )}
+        <Select value={selectedBank} onChange={handleSelectedBank} placeholder="Select Bank" styles={customStyles} options={optionsNG} />
+        <Select value={accountType} onChange={handleAccountType} placeholder="Account Type" styles={customStyles} options={accountTypeOpts} />
         <CustomInput
           placeholder="Account Name"
           showError={errors.accountName ? true : false}
@@ -232,7 +259,7 @@ const AddBankAccount = ({ accountId, getAccount}) => {
           name="sortCode"
           label="sort code"
         />
-           {errors.accountName?.type === 'required' && (
+               {errors.accountName?.type === 'required' && (
           <InputError>Your account name is required</InputError>
         )}
         <CustomInput
@@ -244,18 +271,8 @@ const AddBankAccount = ({ accountId, getAccount}) => {
           name="accountName"
           label="Account Name"
         />
-        {errors.primaryBank?.type === 'required' && (
-          <InputError>Your primary bank name is required</InputError>
-        )}
-        <CustomInput
-          placeholder="Bank Name"
-          showError={errors.primaryBank ? true : false}
-          register={register({
-            required: true,
-          })}
-          name="primaryBank"
-          label="Bank Name"
-        />
+        <Select value={selectedBank} onChange={handleSelectedBank} placeholder="Select Bank" styles={customStyles} options={country === 'GBP' ? optionsUK : optionsEU} />
+        <Select value={accountType} onChange={handleAccountType}  placeholder="Account Type" styles={customStyles} options={accountTypeOpts} />
         <CustomButton loading={false}>Add Bank</CustomButton>
       </form>
     );
@@ -352,18 +369,8 @@ const AddBankAccount = ({ accountId, getAccount}) => {
           name="accountName"
           label="Account Name"
         />
-        {errors.primaryBank?.type === 'required' && (
-          <InputError>Your bank name is required</InputError>
-        )}
-        <CustomInput
-          placeholder="Bank Name"
-          showError={errors.primaryBank ? true : false}
-          register={register({
-            required: true,
-          })}
-          name="primaryBank"
-          label="Bank Name"
-        />
+        <Select value={selectedBank} onChange={handleSelectedBank} placeholder="Select Bank" styles={customStyles} options={country === 'USD' ? optionsUS : optionsCD} />
+        <Select value={accountType} onChange={handleAccountType}  placeholder="Account Type" styles={customStyles} options={accountTypeOpts} />
         <CustomButton loading={false}>Add Bank</CustomButton>
       </form>
     );
